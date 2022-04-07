a0f87b54c06771128af92dbedf56def3a013104e
Asieef <asieef.chowdhury@gmail.com>
2022-04-07 11:55:26 +0600

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'port',
+    title: 'Asieef | Front-end Dev',
     htmlAttrs: {
       lang: 'en',
     },
