7e948004c304d1ad577eb9991b168b8e821f1698
Asieef <asieef.chowdhury@gmail.com>
2022-09-17 14:55:07 +0600

    skills

--- components/Skill.vue
+++ components/Skill.vue
@@ -1,29 +1,49 @@
 <template>
-    <div class="container mx-auto">
-        <div class="mx-4">
-            <div class="flex justify-center items-center mt-10">
-                <h2 class="font-display lg:text-[40px] font-light mr-4 text-[#232323]">Skill</h2>
-                <h2
-                    class="font-display lg:text-[40px] font-semibold text-[#232323] border-b-2 border-[#fecd45]"
-                >Sets</h2>
-            </div>
-            <div class="grid grid-cols-4 lg:h-[400px] h-[180px]">
-                <div class="col-span-1 flex justify-center items-center">
-                    <img src="/html5.png" class="lg:h-24 h-14" />
-                </div>
-
-                <div class="col-span-1 flex justify-center items-center">
-                    <img src="/javascript.png" class="lg:h-24 h-14" />
-                </div>
-
-                <div class="col-span-1 flex justify-center items-center">
-                    <img src="/vue.png" class="lg:h-24 h-14" />
-                </div>
-
-                <div class="col-span-1 flex justify-center items-center">
-                    <img src="/nuxt.png" class="lg:h-24 h-14" />
-                </div>
-            </div>
+  <div class="container mx-auto">
+    <div class="mx-4">
+      <div class="flex justify-center items-center mt-10">
+        <h2 class="font-display lg:text-[40px] font-light mr-4 text-[#232323]">
+          Skill
+        </h2>
+        <h2
+          class="font-display lg:text-[40px] font-semibold text-[#232323] border-b-2 border-[#fecd45]"
+        >
+          Sets
+        </h2>
+      </div>
+      <div class="grid grid-cols-4 lg:h-[400px] h-[180px]">
+        <div class="col-span-1 flex justify-center items-center">
+          <img src="/html5.png" class="lg:h-24 h-14" />
         </div>
+
+        <div class="col-span-1 flex justify-center items-center">
+          <img src="/javascript.png" class="lg:h-24 h-14" />
+        </div>
+
+        <div class="col-span-1 flex justify-center items-center">
+          <img src="/vue.png" class="lg:h-24 h-14" />
+        </div>
+
+        <div class="col-span-1 flex justify-center items-center">
+          <img src="/nuxt.png" class="lg:h-24 h-14" />
+        </div>
+
+        <div class="col-span-1 flex justify-center items-center">
+          <img src="/figma.png" class="lg:h-24 h-14" />
+        </div>
+
+        <div class="col-span-1 flex justify-center items-center">
+          <img src="/tailwind.png" class="lg:h-24 h-14" />
+        </div>
+
+        <div class="col-span-1 flex justify-center items-center">
+          <img src="/bootstrap.png" class="lg:h-24 h-14" />
+        </div>
+
+        <div class="col-span-1 flex justify-center items-center">
+          <img src="/github.png" class="lg:h-24 h-14" />
+        </div>
+      </div>
     </div>
+  </div>
 </template>
