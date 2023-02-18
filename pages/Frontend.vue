0379bb938df653a3d776a9d0ebfa349effdf228a
Asieef <asieef.chowdhury@gmail.com>
2023-02-18 13:07:38 +0600

    notes

--- pages/Frontend.vue
+++ pages/Frontend.vue
@@ -17,7 +17,7 @@
               </h2>
               <p class="text-base text-body-color">
                 These are some small front end projects implemented with Vue.Js
-                framework. Tailwind, Bootstrap has been used for styling
+                framework. Tailwind, Bootstrap have been used for styling
               </p>
             </div>
           </div>
@@ -40,7 +40,7 @@
                 <a
                   href="https://job-listing-beta.vercel.app/"
                   target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition"
+                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
                 >
                   See Demo
                 </a>
@@ -66,7 +66,7 @@
                 <a
                   href="https://asieef.github.io/to-do/"
                   target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition"
+                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
                 >
                   See Demo
                 </a>
@@ -91,7 +91,7 @@
                 <a
                   href="https://asieef.github.io/chart/"
                   target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition"
+                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
                 >
                   See Demo
                 </a>
@@ -116,7 +116,7 @@
                 <a
                   href="https://asieef.github.io/card/"
                   target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition"
+                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
                 >
                   See Demo
                 </a>
@@ -141,7 +141,7 @@
                 <a
                   href="https://asieef.github.io/advice/"
                   target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition"
+                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
                 >
                   See Demo
                 </a>
@@ -170,7 +170,7 @@
                 <a
                   href="https://asieef.github.io/bootstart/"
                   target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition"
+                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
                 >
                   See Demo
                 </a>
@@ -195,7 +195,7 @@
                 <a
                   href="https://asieef.github.io/intro/"
                   target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition"
+                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
                 >
                   See Demo
                 </a>
@@ -221,7 +221,7 @@
                 <a
                   href="https://asieef.github.io/ecom-demo/"
                   target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition"
+                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
                 >
                   See Demo
                 </a>
@@ -230,6 +230,31 @@
           </div>
 
           <!-- Ecom Project Ends -->
+
+          <!-- Notes app starts -->
+          <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+            <div class="relative mb-14 border">
+              <div class="rounded-lg overflow-hidden">
+                <img src="/front/notes.jpg" alt="portfolio" class="w-full" />
+              </div>
+              <div
+                class="text-center bg-white relative z-10 py-2 px-3 rounded mx-7 -mt-20"
+              >
+                <span class="text-sm text-primary font-semibold block mb-2">
+                  Notes App
+                </span>
+
+                <a
+                  href="https://asieef.github.io/notes-live/"
+                  target="blank"
+                  class="w-full bg-slate-100 text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition"
+                >
+                  See Demo
+                </a>
+              </div>
+            </div>
+          </div>
+          <!-- Notes app ends -->
         </div>
       </div>
     </section>
