c606ec7f01a3b2fad5ace9428cc7ed95014d08b6
Asieef <asieef.chowdhury@gmail.com>
2023-04-12 11:01:41 +0600

    db json

--- pages/Frontend.vue
+++ pages/Frontend.vue
@@ -3,260 +3,77 @@
     <HomeBar />
     <!-- ====== Portfolio Section Start -->
     <section class="pt-8 lg:pt-[40px] pb-12 lg:pb-[40px] mx-8">
-      <div class="container mx-auto">
-        <div class="flex flex-wrap -mx-4">
-          <div class="w-full px-4">
-            <div class="text-center mx-auto mb-[60px] max-w-[510px]">
-              <span class="font-semibold text-lg text-primary mb-2 block">
-                Front End Projects
-              </span>
-              <h2
-                class="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4"
-              >
-                Recent Projects
-              </h2>
-              <p class="text-base text-body-color">
-                These are some small front end projects implemented with Vue.Js
-                framework. Tailwind, Bootstrap have been used for styling
-              </p>
-            </div>
+      <div class="flex flex-wrap -mx-4">
+        <div class="w-full px-4">
+          <div class="text-center mx-auto mb-[60px] max-w-[510px]">
+            <span class="font-semibold text-lg text-primary mb-2 block">
+              Front End Projects
+            </span>
+            <h2
+              class="font-bold text-3xl sm:text-4xl md:text-[40px] text-dark mb-4"
+            >
+              Recent Projects
+            </h2>
+            <p class="text-base text-body-color">
+              These are some small front end projects implemented with Vue.Js
+              framework. Tailwind, Bootstrap have been used for styling
+            </p>
           </div>
-        </div>
-
-        <!-- Job Listing Starts -->
-        <div class="flex flex-wrap -mx-4">
-          <div class="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div class="relative mb-14 border">
-              <div class="rounded-lg overflow-hidden">
-                <img src="/front/joblist.jpg" alt="portfolio" class="w-full" />
-              </div>
-              <div
-                class="text-center bg-white relative z-10 py-2 px-3 rounded mx-7 -mt-20"
-              >
-                <span class="text-sm text-primary font-semibold block mb-2">
-                  Job Listing
-                </span>
-
-                <a
-                  href="https://job-listing-beta.vercel.app/"
-                  target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
-                >
-                  See Demo
-                </a>
-              </div>
-            </div>
-          </div>
-          <!-- Job Listing Ends -->
-
-          <!-- To Do App Starts -->
-
-          <div class="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div class="relative mb-14 border">
-              <div class="rounded-lg overflow-hidden">
-                <img src="/front/todo.jpg" alt="portfolio" class="w-full" />
-              </div>
-              <div
-                class="text-center bg-white relative z-10 py-2 px-3 rounded mx-7 -mt-20"
-              >
-                <span class="text-sm text-primary font-semibold block mb-2">
-                  To Do App
-                </span>
-
-                <a
-                  href="https://asieef.github.io/to-do/"
-                  target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
-                >
-                  See Demo
-                </a>
-              </div>
-            </div>
-          </div>
-          <!-- To Do Ends -->
-
-          <!-- Interactive Chart Starts -->
-          <div class="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div class="relative mb-14 border">
-              <div class="rounded-lg overflow-hidden">
-                <img src="/front/chart.jpg" alt="portfolio" class="w-full" />
-              </div>
-              <div
-                class="text-center bg-white relative z-10 py-2 px-3 rounded mx-7 -mt-20"
-              >
-                <span class="text-sm text-primary font-semibold block mb-2">
-                  Interactive Chart
-                </span>
-
-                <a
-                  href="https://asieef.github.io/chart/"
-                  target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
-                >
-                  See Demo
-                </a>
-              </div>
-            </div>
-          </div>
-          <!-- Interactive Chart Ends -->
-
-          <!-- Fetch Profile Starts -->
-          <div class="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div class="relative mb-14 border">
-              <div class="rounded-lg overflow-hidden">
-                <img src="/front/profile.jpg" alt="portfolio" class="w-full" />
-              </div>
-              <div
-                class="text-center bg-white relative z-10 py-2 px-3 rounded mx-7 -mt-20"
-              >
-                <span class="text-sm text-primary font-semibold block mb-2">
-                  Fetch Profile
-                </span>
-
-                <a
-                  href="https://asieef.github.io/card/"
-                  target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
-                >
-                  See Demo
-                </a>
-              </div>
-            </div>
-          </div>
-          <!-- Fetch Profile Ends -->
-
-          <!-- Advice Generator Starts -->
-          <div class="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div class="relative mb-14 border">
-              <div class="rounded-lg overflow-hidden">
-                <img src="/front/advice.jpg" alt="portfolio" class="w-full" />
-              </div>
-              <div
-                class="text-center bg-white relative z-10 py-2 px-3 rounded mx-7 -mt-20"
-              >
-                <span class="text-sm text-primary font-semibold block mb-2">
-                  Advice Genrator
-                </span>
-
-                <a
-                  href="https://asieef.github.io/advice/"
-                  target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
-                >
-                  See Demo
-                </a>
-              </div>
-            </div>
-          </div>
-          <!-- Advice Generator Ends -->
-
-          <!-- Bootstrap Template Starts -->
-          <div class="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div class="relative mb-14 border">
-              <div class="rounded-lg overflow-hidden">
-                <img
-                  src="/front/bootstrap.jpg"
-                  alt="portfolio"
-                  class="w-full"
-                />
-              </div>
-              <div
-                class="text-center bg-white relative z-10 py-2 px-3 rounded mx-7 -mt-20"
-              >
-                <span class="text-sm text-primary font-semibold block mb-2">
-                  Bootstrap Template
-                </span>
-
-                <a
-                  href="https://asieef.github.io/bootstart/"
-                  target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
-                >
-                  See Demo
-                </a>
-              </div>
-            </div>
-          </div>
-          <!-- Bootstrap Template Ends -->
-
-          <!-- Intro Project Starts -->
-          <div class="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div class="relative mb-14 border">
-              <div class="rounded-lg overflow-hidden">
-                <img src="/front/intro.jpg" alt="portfolio" class="w-full" />
-              </div>
-              <div
-                class="text-center bg-white relative z-10 py-2 px-3 rounded mx-7 -mt-20"
-              >
-                <span class="text-sm text-primary font-semibold block mb-2">
-                  Intro - Tailwind + Vue
-                </span>
-
-                <a
-                  href="https://asieef.github.io/intro/"
-                  target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
-                >
-                  See Demo
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <!-- Intro Project Ends -->
-
-          <!-- Ecom Project Starts -->
-          <div class="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div class="relative mb-14 border">
-              <div class="rounded-lg overflow-hidden">
-                <img src="/front/ecom.jpg" alt="portfolio" class="w-full" />
-              </div>
-              <div
-                class="text-center bg-white relative z-10 py-2 px-3 rounded mx-7 -mt-20"
-              >
-                <span class="text-sm text-primary font-semibold block mb-2">
-                  Ecom Product - Add to Cart
-                </span>
-
-                <a
-                  href="https://asieef.github.io/ecom-demo/"
-                  target="blank"
-                  class="w-full text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition bg-slate-100"
-                >
-                  See Demo
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <!-- Ecom Project Ends -->
-
-          <!-- Notes app starts -->
-          <div class="w-full md:w-1/2 xl:w-1/3 px-4">
-            <div class="relative mb-14 border">
-              <div class="rounded-lg overflow-hidden">
-                <img src="/front/notes.jpg" alt="portfolio" class="w-full" />
-              </div>
-              <div
-                class="text-center bg-white relative z-10 py-2 px-3 rounded mx-7 -mt-20"
-              >
-                <span class="text-sm text-primary font-semibold block mb-2">
-                  Notes App
-                </span>
-
-                <a
-                  href="https://asieef.github.io/notes-live/"
-                  target="blank"
-                  class="w-full bg-slate-100 text-body-color text-sm font-semibold py-2 px-7 inline-block border rounded-md hover:bg-primary hover:border-primary hover:bg-[#FECD45] hover:text-white transition"
-                >
-                  See Demo
-                </a>
-              </div>
-            </div>
-          </div>
-          <!-- Notes app ends -->
         </div>
       </div>
+
+      <!-- ====== Cards Section Start -->
+      <section class="bg-[#F3F4F6] pt-20 pb-10 lg:pt-[120px] lg:pb-20">
+        <div class="container mx-auto">
+          <div class="-mx-4 flex flex-wrap">
+            <div
+              v-for="project in projects"
+              :key="project.id"
+              class="w-full px-4 md:w-1/2 xl:w-1/3"
+            >
+              <div class="mb-10 overflow-hidden rounded-lg bg-white">
+                <img :src="project.banner" alt="image" class="w-full" />
+                <div class="p-8 text-center sm:p-9 md:p-7 xl:p-9">
+                  <h3>
+                    <a
+                      class="text-dark hover:text-primary mb-4 block text-xl font-semibold sm:text-[22px] md:text-xl lg:text-[22px] xl:text-xl 2xl:text-[22px]"
+                    >
+                      {{ project.title }}
+                    </a>
+                  </h3>
+                  <p class="text-body-color mb-7 text-base leading-relaxed">
+                    {{ project.description }}
+                  </p>
+                  <a
+                    :href="project.link"
+                    class="text-body-color hover:border-primary hover:bg-primary inline-block rounded-full border border-[#E5E7EB] py-2 px-7 text-base font-medium transition hover:text-gray-600"
+                  >
+                    View Details
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- ====== Cards Section End -->
     </section>
   </div>
 </template>
+
+<script>
+import projects from '../data/db.json'
+
+export default {
+  data() {
+    return {
+      projects: [],
+    }
+  },
+
+  methods: {},
+  mounted() {
+    this.projects = projects
+  },
+}
+</script>
