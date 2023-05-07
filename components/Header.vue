07306045d8f1d37ac80bd04324c1953e18cfb1c5
Asieef <asieef.chowdhury@gmail.com>
2023-05-07 13:30:18 +0600

    meta

--- components/Header.vue
+++ components/Header.vue
@@ -1,37 +1,52 @@
 <template>
-    <div>
-        <div
-            class="grid lg:grid-cols-6 grid-cols-1 gap-6 lg:h-screen bg-cover bg-no-repeat bg-top bg-fixed justify-items-center content-center"
-            style="background-image: url('/h-bg.jpg')"
-        >
-            <div class="col-span-2 mt-8">
-                <img src="/avatar-1.png" class="h-56 lg:h-64" />
-            </div>
+  <div>
+    <div
+      class="grid lg:grid-cols-6 grid-cols-1 gap-6 lg:h-screen bg-cover bg-no-repeat bg-top bg-fixed justify-items-center content-center"
+      style="background-image: url('/h-bg.jpg')"
+    >
+      <div class="col-span-2 mt-8">
+        <img src="/avatar-1.png" class="h-56 lg:h-64" />
+      </div>
 
-            <div class="col-span-4 tracking-widest indent-2 flex justify-center items-center">
-                <div class="flex-row">
-                    <div class="flex">
-                        <h2
-                            class="font-display lg:text-[80px] text-[30px] font-light lg:mr-4 text-[#232323]"
-                        >I'm</h2>
-                        <h2
-                            class="font-display lg:text-[80px] text-[30px] font-semibold text-[#232323] border-b-2 border-[#fecd45]"
-                        >asieef kabir</h2>
-                    </div>
-                    <div class="flex justify-center">
-                        <h2 class="font-display text-xs font-medium text-gray-500 uppercase py-2">
-                            Designer & Frontend
-                            Developer
-                        </h2>
-                    </div>
-                </div>
+      <div
+        class="col-span-4 tracking-widest indent-2 flex justify-center items-center"
+      >
+        <div class="flex-row">
+          <div class="flex">
+            <h2
+              class="font-display lg:text-[80px] text-[30px] font-light lg:mr-4 text-[#232323]"
+            >
+              I'm
+            </h2>
+            <h2
+              class="font-display lg:text-[80px] text-[30px] font-semibold text-[#232323] border-b-2 border-[#fecd45]"
+            >
+              asieef kabir
+            </h2>
+          </div>
+          <div class="lg:flex justify-center items-center">
+            <h2
+              class="font-display text-xs font-medium text-gray-500 uppercase py-2"
+            >
+              Designer & Frontend Developer |
+            </h2>
+            <div class="flex">
+              <img class="h-3 pl-2" src="/meta.png" />
+              <p
+                class="font-display text-xs font-medium text-gray-500 uppercase"
+              >
+                certified
+              </p>
             </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style>
 .font-display {
-    font-family: "Montserrat";
+  font-family: 'Montserrat';
 }
 </style>
